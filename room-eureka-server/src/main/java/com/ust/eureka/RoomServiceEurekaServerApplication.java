9f976a43f37524122a0147fd7d2de5a1fbc2d734
Sriharsha Bopparaju <Sriharsha3006@gmail.com>
2018-04-04 14:45:05 -0700

    updated properties ans RoomService..App file

--- room-eureka-server/src/main/java/com/ust/eureka/RoomServiceEurekaServerApplication.java
+++ room-eureka-server/src/main/java/com/ust/eureka/RoomServiceEurekaServerApplication.java
@@ -1,10 +1,12 @@
 package com.ust.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RefreshScope
 public class RoomServiceEurekaServerApplication {
 
 	public static void main(String[] args) {
